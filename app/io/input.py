50ce5059cb68d733d7f7d3cf897562b6cec1ff39
Bohdan Liubar <juhim.fed@gmail.com>
2024-04-11 20:44:32 +0300

    Added some empty functions

---
+++ app/io/input.py
@@ -0,0 +1,5 @@
+def input_text_from_console():
+
+def read_text_from_file(filename):
+
+def read_text_from_file_with_pandas(filename):
---
+++ app/io/output.py
@@ -0,0 +1,3 @@
+def print_to_console(text):
+
+def write_text_to_file(text, filename):
